354f491f55169b42b725efced51851e5cc1824be
Jorge C <jorgec@Jorges-MacBook-Pro-2.local>
2020-10-10 10:54:15 -0700

    add js to navbar-items

--- index.js
+++ index.js
@@ -56,3 +56,9 @@ image.forEach(img => {
   //   alert('Hello!');
   // }
 
+// if items not showing then show else remove
+$(document).on('click','.navbar-collapse',function(e) {
+  if( $(e.target).is('a:not(".dropdown-toggle")') ) {
+      $(this).collapse('hide');
+  }
+});
